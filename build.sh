a62b1ede6e2f66899febe6ca0ff3396c9323351f
Tomas Petricek <tomas@tomasp.net>
2023-03-27 09:47:06 +0200

    Add all files and test

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+rm -rf .output/
 ls *.md | sed 's/\.md//g' | awk '{ print "- ["$0"]("$0".html)" }' > index.md
 REPO=$(git remote -v | head -n 1 | cut -f 2 | cut -d ' ' -f 1)
-
+echo "Cloning $REPO"
+git clone --branch gh-pages $REPO .output
+for MD in *.md; do
+	HTML=${MD%.*}.html
+	pandoc --template template.html $MD > .output/$HTML
+done
+cd .output
+git add .
+git commit -m "Updating web page"
+git push origin gh-pages
+cd ..
---
+++ test.md
@@ -0,0 +1,3 @@
+# This is test
+
+Does it _just_ work when we a **new file**?
